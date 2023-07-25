1519a83c5d46c30dea54cd0a2221bf4b8cd7d167
MarioSantoro <mariosantoro124@gmail.com>
2023-07-25 16:19:54 +0200

    added new route when click image the page change and we see the singleComic

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -3,16 +3,15 @@
 
 @section('main-content')
     <div class="container mt-5">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center align-items-center">
-                <div class="card">
-                    <img class="card-img-top" src="{{ $singleComic->thumb }}" alt="{{ $singleComic->title }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $singleComic->title }}</h5>
-                        <p class="card-text">{{ $singleComic->description }}</p>
-                    </div>
-                </div>
+        <div class="row  d-flex flex-column align-items-center">
+            <div class="col-cs">
+                <img class="" src="{{ $singleComic->thumb }}" alt="{{ $singleComic->title }}">
             </div>
         </div>
+        <div class="text-white">
+            <h5 class="">{{ $singleComic->title }}</h5>
+            <p class="">{{ $singleComic->description }}</p>
+        </div>
+    </div>
     </div>
 @endsection
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,18 +3,13 @@
 
 @section('main-content')
     <div class="container mt-5">
-        <div class="row">
-            <div class="col-12 d-flex flex-wrap">
-                @foreach ($comics as $comic)
-                    <div class="card">
-                        <img class="card-img-top" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $comic->title }}</h5>
-                            <p class="card-text">{{ $comic->description }}</p>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+        <div class="row d-flex flex-wrap">
+            @foreach ($comics as $comic)
+                <div class="col-cs">
+                    <a href="{{ route('showComic', $comic->id) }}"><img class="" src="{{ $comic->thumb }}"
+                            alt="{{ $comic->title }}"></a>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
